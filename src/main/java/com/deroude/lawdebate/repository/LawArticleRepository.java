72d2439f907080c20e2b339e275489194b410c3d
gabriel.deroude <deroude@gmail.com>
2017-03-04 22:37:11 +0200

    revert

--- src/main/java/com/deroude/lawdebate/repository/LawArticleRepository.java
+++ src/main/java/com/deroude/lawdebate/repository/LawArticleRepository.java
@@ -21,14 +21,15 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface LawArticleRepository extends JpaRepository<LawArticle, Long> {
+//where (:search is null or l.agency like :search or v.title like :search)
 
     @Query("select new com.deroude.lawdebate.dto.LawArticleSummary (l,v,count(y),count(n),count(a)) from ArticleVersion v "
 	    + "join v.article l on v.last=1 "
 	    + "left join l.responses yr on yr.position = 'YAY' left join yr.votes y "
 	    + "left join l.responses nr on nr.position='NAY' left join nr.votes n "
 	    + "left join l.responses ar on ar.position='ABSTAIN' left join ar.votes a "
-	    + "where (:search is null or l.agency like :search or v.title like :search) group by l.id")
-    public Page<LawArticleSummary> getArticles(@Param("search") String search, Pageable preq);
+	    + " group by l.id")
+    public Page<LawArticleSummary> getArticles(Pageable preq);
 
     @Query("select new com.deroude.lawdebate.dto.LawArticleSummary (l,v,count(y),count(n),count(a)) from ArticleVersion v "
 	    + "join v.article l on v.last=1 "
